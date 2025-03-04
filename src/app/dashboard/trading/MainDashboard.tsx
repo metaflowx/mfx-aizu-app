2969e661497866c4de05b3e2280d80a82e25cc0c
cpnsing <cpnsingh8181@gmail.com>
2025-03-04 15:01:43 +0530

    connect button,apply button and table data and name updated

--- src/app/dashboard/trading/MainDashboard.tsx
+++ src/app/dashboard/trading/MainDashboard.tsx
@@ -1,38 +1,34 @@
-import { StatsCard } from '@/components/stats-card'
-import React from 'react'
+import { StatsCard } from "@/components/stats-card";
+import React from "react";
 
 export default function MainDashboard() {
-    const dashboardData=[
-        {
-            title:"Balance",
-            value:"$23,768"
-        },
-        {
-            title:"Today's Profit",
-            value:"$23,768"
-        },
-        {
-            title:"Coins",
-            value:"",
-            isCoin:true
-        },
-    ]
+  const dashboardData = [
+    {
+      title: "Total Profit",
+      value: "$23,768",
+    },
+    {
+      title: "Today's Profit",
+      value: "$23,768",
+    },
+    {
+      title: "Coins",
+      value: "",
+      isCoin: true,
+    },
+  ];
   return (
-    <div className='grid grid-cols-1  sm:grid-cols-3 gap-4'>
-
-        {dashboardData.map((item)=>{
-            return(
-                <StatsCard
-                          title={item.title}
-                          value={item.value}
-                          subValue=""
-                          isCoin={item.isCoin}
-                        />
-            )
-        })}
-
-
-      
+    <div className="grid grid-cols-1  sm:grid-cols-3 gap-4">
+      {dashboardData.map((item) => {
+        return (
+          <StatsCard
+            title={item.title}
+            value={item.value}
+            subValue=""
+            isCoin={item.isCoin}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
--- src/app/dashboard/trading/page.tsx
+++ src/app/dashboard/trading/page.tsx
@@ -18,14 +18,22 @@ import PriceStatics from "./innercard/PriceStatics"
 import TradingHistory from "./innercard/TradingHistory"
 import MainDashboard from "./MainDashboard"
 import MainChart from "./MainChart"
+import CommonButton from "@/components/ui/CommonButton"
 
 export default function TradingPage() {
   return (
     <>
     
-
+<div className="pb-2">
+<CommonButton title="Connect with Binance" width="280px" />
+</div>
       <MainDashboard />
+      <div className="flex items-center py-2" >
+      <h3>Apply for Bot access Whitelist </h3>&nbsp;
+      <CommonButton title="Apply" width="150px" />
 
+      </div>
+    
       <MainChart />
 
       
